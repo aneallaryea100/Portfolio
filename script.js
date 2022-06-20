70e78c556bd857e6d1fb708d894bf857c2e09293
Aneal Laryea <aneallaryea100@gmail.com>
2022-06-20 13:39:46 -0700

    Add Javascript to project

---
+++ script.js
@@ -0,0 +1,4 @@
+const hambx = document.querySelector('.hambx');
+const close = document.querySelector('.close');
+const nav = document.querySelector('.navvme');
+const navLink = document.querySelectorAll('.navlinks');
