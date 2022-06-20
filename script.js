7ab1ffdd9ab99b4148bb245207d9551af8190317
Aneal Laryea <aneallaryea100@gmail.com>
2022-06-20 14:15:58 -0700

    Mobile menu working

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const close = document.querySelector('.close');
 const nav = document.querySelector('.navvme');
 const navLink = document.querySelectorAll('.navlinks');
 hambx.addEventListener('click', () => {
-	nav.classList.add('active');
-	});
+  nav.classList.add('active');
+});
 
-	close.addEventListener('click', () => {
-	nav.classList.remove('active');
-	});
+close.addEventListener('click', () => {
+  nav.classList.remove('active');
+});
 
-	navLink.forEach((element) => element.addEventListener('click', () => {
-	nav.classList.remove('active');
-	}));
+navLink.forEach((element) => element.addEventListener('click', () => {
+  nav.classList.remove('active');
+}));
